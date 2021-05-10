5f1ab8070e9dccb5d264d43fbdcf69ee83474b60
Ravi Masarapu <53607885+RJravi565@users.noreply.github.com>
2021-05-10 10:54:46 +0530

    my project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import React, { Component } from "react";
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+/*function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -20,6 +21,33 @@ function App() {
       </header>
     </div>
   );
+} */
+class App extends Component{
+constructor(props) {
+  super(props);
+  this.state = { apiResponse: "" };
 }
 
+callAPI() {
+  fetch("http://localhost:9000/testAPI")
+      .then(res => res.text())
+      .then(res => this.setState({ apiResponse: res }));
+}
+
+componentWillMount() {
+  this.callAPI();
+}
+
+render() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo"/>
+        <h1 className="App-title"> Welcome To Aspire</h1>
+      </header>
+      <p className="App-intro">;{this.state.apiResponse}</p>
+    </div>
+  );
+}
+}
 export default App;
